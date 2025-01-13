8af28fac148c98bc2103f6357c3011c67af49f2e
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-01-14 00:05:34 +0100

    first commit - project structure

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,5 @@
+import "../styles/global.css"; // Import des styles globaux
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Bienvenue sur Whispy !</h1>
+            <p>Ceci est la landing page de votre application Next.js.</p>
+        </div>
+    );
+}
